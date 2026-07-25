4c14ef04efc8ce57647ba60a6308023432ba68a9
aresden113 <aresden113@gmail.com>
2026-07-24 21:45:05 -0300

    project: final1

--- src/sections/Hero/HeroImage.tsx
+++ src/sections/Hero/HeroImage.tsx
@@ -1,10 +1,10 @@
-import profolio from "@/assets/images/porfolio.png";
+import perfil from "@/assets/images/perfil.png";
 
 export function HeroImage() {
   return (
     <div className="flex justify-center items-center">
       <img
-        src={profolio}
+        src={perfil}
         alt="Julio Gonzalez"
          className="w-72 h-72 rounded-full border-4 border-sky-600 mb-4"
         
